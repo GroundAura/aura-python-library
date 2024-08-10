172012d334d1e9732c1e70cfb2aba875c5a209e9
GroundAura <52499897+GroundAura@users.noreply.github.com>
2024-08-10 03:29:23 -0600

    remove redundant requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     LONG_DESCRIPTION = "\n" + fh.read()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Various functions to simplify common tasks'
 
 setup(
@@ -18,9 +18,9 @@ setup(
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
-	license='Clear BSD',
+    license='Clear BSD',
     packages=find_packages(),
-    install_requires=['configparser', 'os', 'shutil', 'subprocess'],
+    install_requires=[],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
